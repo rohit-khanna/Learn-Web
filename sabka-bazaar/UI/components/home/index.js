69578c32a307f4fcfddef8873ddc3ad9883aceb1
Rohit LIVE <therohitkhanna@live.com>
2018-12-21 15:34:52 +0530

    Added Header + Corousal

---
+++ sabka-bazaar/UI/components/home/index.js
@@ -0,0 +1,16 @@
+// async function getf() {
+//   let obj = require("./model/ShoppingCart");
+//   let inst = await obj.GetCartInstanceAsync();
+
+//  inst.api.fetchProductDetails.call(inst);
+// }
+
+// getf();
+
+$().ready(function() {
+//     console.log('HERE');
+    
+//   $(".corousal__images li").on("mouseover", function() {
+//     console.log("yes");
+//   });
+});
--- sabka-bazaar/UI/index.js
+++
@@ -1,9 +0,0 @@
-async function getf() {
-  let obj = require("./model/ShoppingCart");
-  let inst = await obj.GetCartInstanceAsync();
-
-
- inst.api.fetchProductDetails.call(inst);
-}
-
-getf();
