26f55b9e2e3f9cf2a8a64353511f97a506362ca4
Rohit LIVE <therohitkhanna@live.com>
2018-12-31 16:25:38 +0530

    removed mock data for products

--- sabka-bazaar/webpack.config.js
+++ sabka-bazaar/webpack.config.js
@@ -17,14 +17,6 @@ module.exports = {
   devtool: "source-map",
   module: {
     rules: [
-      //   {
-      //     test: /\.scss$/,
-      //     use: [
-      //       "style-loader", // creates style nodes from JS strings
-      //       "css-loader", // translates CSS into CommonJS
-      //       "sass-loader" // compiles Sass to CSS, using Node Sass by default
-      //     ]
-      //   },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
