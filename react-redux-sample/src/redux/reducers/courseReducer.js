3a6c7d67de609551ec0db493ac1643d55fc578d4
Rohit Khanna <rohit.khanna@nagarro.com>
2019-07-23 15:53:09 +0530

    replaced ACTION-TYPES from strings to constants

--- react-redux-sample/src/redux/reducers/courseReducer.js
+++ react-redux-sample/src/redux/reducers/courseReducer.js
@@ -1,3 +1,5 @@
+import { CREATE_COURSE } from "../actions/actionTypes";
+
 /**
  * Course Reducer
  * @param {*} state initialized to empty array as it will store array of courses 
@@ -5,7 +7,7 @@
  */
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case CREATE_COURSE:
       return [...state, { ...action.course }];
 
     default:
